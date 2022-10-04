338898de557efa8cc3cbb68cb6aadc5f518b7fbb
GNYDSummer <1031988052@qq.com>
2022-10-04 20:48:45 +0800

    modify

---
+++ .doom.d.bak/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
